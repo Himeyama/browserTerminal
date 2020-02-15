b5d4b635d00f8dbe210a84cbcbacff2f48ce516f
MURATA Mitsuharu <hikari.photon@gmail.com>
2020-02-15 09:37:25 +0900

    new

--- browserterminal.js
+++ browserterminal.js
@@ -1,4 +1,8 @@
 class Terminal extends Window{
+    terminalDisplay = ""
+    input = ""
+    ps1 = "$ "
+
     static create(id){
         let terminal
         let window = Window.create(id)
@@ -7,32 +11,98 @@ class Terminal extends Window{
         terminal = new Terminal(window)
         terminal.element = window.element
         terminal.id = window.id
-        
-        document.body.onclick = function(e){
-            if(e.target.parentElement.className == "winTitle"){
-                Terminal.activeID = e.target.parentElement.parentElement.id
-            }else{
-                Terminal.activeID = e.target.parentElement.id
-            }
-        }
-
+        Terminal.list[Terminal.list.length] = terminal
+        terminal.element.children[1].children[0].innerHTML = `${terminal.terminalDisplay}${terminal.ps1}${terminal.input}`
         return terminal
     }
+
+    static get(id){
+        let obj
+        for(let i = 0; i < Terminal.list.length; i++){
+            if(Terminal.list[i].id == id){
+                obj = Terminal.list[i]
+                break
+            }
+        }
+        return obj
+    }
 }
+Terminal.list = []
 
 document.addEventListener("keydown", function(e){
-    let str = document.getElementById(Terminal.activeID).getElementsByClassName("winContents")[0].getElementsByTagName("p")[0].innerHTML
-    if(e.key == "Backspace"){
-        document.getElementById(Terminal.activeID).getElementsByClassName("winContents")[0].innerHTML
-        = `<p style="margin:0;">${str.slice(0, -1)}</p>`
-    }else if(e.key == "Enter"){
-        // str += "<br>"
-        // terminal.setContents(`<p style="margin:0;">${str}</p>`)
-    }else{
-        document.getElementById(Terminal.activeID).getElementsByClassName("winContents")[0].innerHTML
-        = `<p style="margin:0;">${str}${e.key}</p>`
+    let flag = false
+    for(let i = 0; i < Terminal.list.length; i++){
+        if(Terminal.list[i].id == Window.activeID){
+            flag = true
+            break
+        }
+    }
+    if(flag){
+        let obj = Terminal.get(Terminal.activeID)
+        let str = obj.input
+        if(e.key == "Backspace"){
+            // document.getElementById(Terminal.activeID).getElementsByClassName("winContents")[0].innerHTML
+            obj.input = `${str.slice(0, -1)}`
+        }
+        obj.element.children[1].children[0].innerHTML = `${obj.terminalDisplay}${obj.ps1}${obj.input}`
+    }
+})
+
+document.addEventListener("keypress", function(e){
+    let flag = false
+    for(let i = 0; i < Terminal.list.length; i++){
+        if(Terminal.list[i].id == Window.activeID){
+            flag = true
+            break
+        }
+    }
+    // console.log(e)
+    if(flag){
+        let obj = Terminal.get(Terminal.activeID)
+        let str = obj.input
+
+        if(true){
+            if(e.key == "Backspace"){
+                // document.getElementById(Terminal.activeID).getElementsByClassName("winContents")[0].innerHTML
+                obj.input = `${str.slice(0, -1)}`
+            }else if(e.key == "Enter"){
+                let output
+                let skip = false
+                try{
+                    eval(obj.input)
+                    console.log(2)
+                }catch(err){
+                    console.log(1)
+                    skip = true
+                }
+
+                if(!skip && !obj.input == ""){
+                    if(obj.input == "clear"){
+                        obj.terminalDisplay = ""
+                        obj.input = ""
+                    }else{
+                        output = String(eval(obj.input))
+                    }
+                }else if(obj.input == ""){
+                    output = ""
+                }else{
+                    output = `Error: <span style="color:red;">${obj.input}</span>`
+                }
+                obj.terminalDisplay = obj.terminalDisplay + obj.ps1 + obj.input + "<br>" + output + "<br>"
+                obj.input = ""
+                // str += "<br>"
+                // terminal.setContents(`<p style="margin:0;">${str}</p>`)
+            }else{
+                // document.getElementById(Terminal.activeID).getElementsByClassName("winContents")[0].innerHTML
+                obj.input = `${str}${e.key}`
+            }
+        }
+        obj.element.children[1].children[0].innerHTML = `${obj.terminalDisplay}${obj.ps1}${obj.input}`
     }
 })
 
 ter = Terminal.create("terminal")
 ter.setTitle("Terminal")
+
+ter2 = Terminal.create("terminal2")
+ter2.setTitle("Terminal2")
